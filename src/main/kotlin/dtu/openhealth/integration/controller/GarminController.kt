fdfe20775f08cc166a8120d7345c633815b0cf8e
ChristofferSvendsen <s145089@student.dtu.dk>
2020-03-04 19:03:21 +0100

    Added try/catch in Garmin and updated test

--- src/main/kotlin/dtu/openhealth/integration/controller/GarminController.kt
+++ src/main/kotlin/dtu/openhealth/integration/controller/GarminController.kt
@@ -1,8 +1,6 @@
 package dtu.openhealth.integration.controller
 
-import com.google.gson.Gson
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
+import com.google.gson.*
 import dtu.openhealth.integration.data.garmin.BodyCompositionSummaryGarmin
 import dtu.openhealth.integration.data.garmin.DailySummaryGarmin
 import org.springframework.http.HttpStatus
@@ -21,20 +19,30 @@ class GarminController {
     //TODO: Have to add that this is forwarded to a class that handles transfering to OMH and then forward to Kafka Stream
 
     @PostMapping(path = ["/body"])
-    fun bodyCompositionSummary(@RequestBody jsonObject: String): ResponseEntity<String> {
-        logger.info("Received body data: $jsonObject")
-        val obj: JsonObject = Gson().fromJson(jsonObject, JsonObject::class.java)
-        val objArray: JsonArray = Gson().fromJson(obj.get("body").asJsonArray, JsonArray::class.java)
-        for (jsonObj in objArray) println(Gson().fromJson(jsonObj, BodyCompositionSummaryGarmin::class.java))
+    fun bodyCompositionSummary(@RequestBody data: String): ResponseEntity<String> {
+        logger.info("Received body data: $data")
+        try {
+            val jsonObject: JsonObject = Gson().fromJson(data, JsonObject::class.java)
+            val jsonArray: JsonArray = Gson().fromJson(jsonObject.get("body").asJsonArray, JsonArray::class.java)
+            for (jsonElement: JsonElement in jsonArray) println(Gson().fromJson(jsonElement, BodyCompositionSummaryGarmin::class.java))
+        } catch (e: JsonSyntaxException) {
+            logger.error(e.message)
+            return ResponseEntity("Json Syntax error", HttpStatus.BAD_REQUEST)
+        }
         return ResponseEntity(HttpStatus.CREATED)
     }
 
     @PostMapping(path = ["/daily"])
     fun dailySummary(@RequestBody jsonObject: String): ResponseEntity<String> {
         logger.info("Received daily data: $jsonObject")
-        val obj: JsonObject = Gson().fromJson(jsonObject, JsonObject::class.java)
-        val objArray: JsonArray = Gson().fromJson(obj.get("dailies").asJsonArray, JsonArray::class.java)
-        for (jsonObj in objArray) println(Gson().fromJson(jsonObj, DailySummaryGarmin::class.java))
+        try {
+            val obj: JsonObject = Gson().fromJson(jsonObject, JsonObject::class.java)
+            val objArray: JsonArray = Gson().fromJson(obj.get("dailies").asJsonArray, JsonArray::class.java)
+            for (jsonObj in objArray) println(Gson().fromJson(jsonObj, DailySummaryGarmin::class.java))
+        } catch (e: JsonSyntaxException) {
+            logger.error(e.message)
+            return ResponseEntity("Json Syntax error", HttpStatus.BAD_REQUEST)
+        }
         return ResponseEntity(HttpStatus.CREATED)
     }
 }
--- src/test/kotlin/dtu/openhealth/integration/garmin/GarminBodyTest.kt
+++ src/test/kotlin/dtu/openhealth/integration/garmin/GarminBodyTest.kt
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GarminBodyTest(@Autowired val testRestTemplate: TestRestTemplate) {
 
-    val jsonString ="""
+    val validJsonString ="""
     {
         "body":
         [
@@ -30,10 +30,37 @@ class GarminBodyTest(@Autowired val testRestTemplate: TestRestTemplate) {
             ]
     }"""
 
+    val invalidJsonString ="""
+    {
+        "body":
+        [
+            {
+                "userId": "4aacafe82427c251df9c9592d0c06768", 
+                "userAccessToken": "8f57a6f1-26ba-4b05-a7cd-c6b525a4c7a2", 
+                "summaryId": "EXAMPLE_678901", 
+                "measurementTimeInSeconds": 1439741130, 
+                "measurementTimeOffsetInSeconds": 0, 
+                "muscleMassInGrams": 25478
+                "boneMassInGrams": 2437
+                "bodyWaterInPercent": 59.4, 
+                "bodyFatInPercent": 17.1, 
+                "bodyMassIndex": 23.2, 
+                "weightInGrams": 75450
+            }
+            ]
+    }"""
+
     @Test
-    fun testBodyEndpoint() {
-        val result = testRestTemplate.postForEntity("/api/garmin/body", jsonString, String::class.java)
+    fun testBodyEndpointValid() {
+        val result = testRestTemplate.postForEntity("/api/garmin/body", validJsonString, String::class.java)
         assertThat(result.statusCode).isEqualTo(HttpStatus.CREATED)
+    }
+
+    @Test
+    fun testBodyEndpointSyntaxError() {
+        val result = testRestTemplate.postForEntity("/api/garmin/body", invalidJsonString, String::class.java)
+        assertThat(result.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+        assertThat(result.body).isEqualTo("Json Syntax error")
     }
 
 
