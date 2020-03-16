ab306c8e4397fc05ce5c41a02722495376182d3b
ChristofferSvendsen <s145089@student.dtu.dk>
2020-03-16 09:10:43 +0100

    removed mysql as dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("mysql:mysql-connector-java:8.0.19")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
