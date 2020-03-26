ddf1255e3ac7b1fd167435735146e48df0d20fa4
Simon <simon_petersen1994@hotmail.com>
2020-03-26 10:36:25 +0100

    Test of FitbitActivity mapping.

--- src/test/kotlin/dtu/openhealth/integration/fitbit/mapping/FitbitActivityMappingTest.kt
+++ src/test/kotlin/dtu/openhealth/integration/fitbit/mapping/FitbitActivityMappingTest.kt
@@ -5,8 +5,12 @@ import dtu.openhealth.integration.data.fitbit.FitbitActivity
 import dtu.openhealth.integration.data.fitbit.FitbitActivitySummary
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.openmhealth.schema.domain.omh.*
+import java.math.BigDecimal
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.LocalTime
+import java.time.ZoneOffset
 
 class FitbitActivityMappingTest {
 
@@ -18,6 +22,10 @@ class FitbitActivityMappingTest {
         val omhData = fitbitSummary.mapToOMH()
 
         assertThat(omhData.size).isEqualTo(expectedOmhElements)
+
+        validateHeartRate(omhData, activitySummary)
+        validateCalories(omhData, activitySummary)
+        validateStepCount(omhData, activitySummary)
     }
 
     @Test
@@ -30,6 +38,79 @@ class FitbitActivityMappingTest {
         val omhData = fitbitSummary.mapToOMH()
 
         assertThat(omhData.size).isEqualTo(expectedOmhElements)
+
+        validateHeartRate(omhData, activitySummary)
+        validateCalories(omhData, activitySummary)
+        validateStepCount(omhData, activitySummary)
+        validateActivity(omhData, activity)
+    }
+
+    @Test
+    fun testFitbitActivityMapping_WithDistance() {
+        val activity = prepareActivityWithDistance()
+        val omhData = activity.mapToOMH()
+
+        assertThat(omhData).isNotNull
+
+        validateActivityWithDistance(listOf(omhData), activity)
+    }
+
+    private fun validateHeartRate(omhData: List<Measure>, summary: FitbitActivitySummary) {
+        val heartRateList = omhData.filterIsInstance<HeartRate>()
+        val expectedElements = 1
+        assertThat(heartRateList.size).isEqualTo(expectedElements)
+
+        val heartRate = heartRateList[0]
+        assertThat(heartRate.heartRate?.value?.longValueExact()).isEqualTo(summary.restingHeartRate)
+        assertThat(heartRate.temporalRelationshipToPhysicalActivity).isEqualTo(TemporalRelationshipToPhysicalActivity.AT_REST)
+    }
+
+    private fun validateStepCount(omhData: List<Measure>, summary: FitbitActivitySummary) {
+        val stepCountList = omhData.filterIsInstance<StepCount2>()
+        val expectedElements = 1
+        assertThat(stepCountList.size).isEqualTo(expectedElements)
+
+        val stepCount = stepCountList[0]
+        assertThat(stepCount.stepCount.longValueExact()).isEqualTo(summary.steps)
+    }
+
+    private fun validateCalories(omhData: List<Measure>, summary: FitbitActivitySummary) {
+        val caloriesList = omhData.filterIsInstance<CaloriesBurned2>()
+        val expectedElements = 1
+        assertThat(caloriesList.size).isEqualTo(expectedElements)
+
+        val caloriesBurned = caloriesList[0]
+        assertThat(caloriesBurned.kcalBurned.value.longValueExact()).isEqualTo(summary.caloriesOut)
+    }
+
+    private fun validateActivity(omhData: List<Measure>, activity: FitbitActivity) {
+        val activitiesList = omhData.filterIsInstance<PhysicalActivity>()
+        val expectedElements = 1
+        assertThat(activitiesList.size).isEqualTo(expectedElements)
+
+        val physicalActivity = activitiesList[0]
+        validatePhysicalActivity(physicalActivity, activity)
+    }
+
+    private fun validateActivityWithDistance(omhData: List<Measure>, activity: FitbitActivity) {
+        val activitiesList = omhData.filterIsInstance<PhysicalActivity>()
+        val expectedElements = 1
+        assertThat(activitiesList.size).isEqualTo(expectedElements)
+
+        val physicalActivity = activitiesList[0]
+        assertThat(physicalActivity.distance).isNotNull
+        assertThat(physicalActivity.distance?.value).isEqualTo(BigDecimal.valueOf(activity.distance!!))
+        validatePhysicalActivity(physicalActivity, activity)
+    }
+
+    private fun validatePhysicalActivity(physicalActivity: PhysicalActivity, activity: FitbitActivity) {
+        assertThat(physicalActivity.activityName).isEqualTo(activity.name)
+        assertThat(physicalActivity.caloriesBurned.value.longValueExact()).isEqualTo(activity.calories)
+
+        val startDateTime = LocalDateTime.of(2020,3,24,8,10).atOffset(ZoneOffset.UTC)
+        assertThat(physicalActivity.effectiveTimeFrame.timeInterval.startDateTime).isEqualTo(startDateTime)
+        assertThat(physicalActivity.effectiveTimeFrame.timeInterval.duration.value.longValueExact()).isEqualTo(activity.duration)
+        assertThat(physicalActivity.effectiveTimeFrame.timeInterval.duration.typedUnit).isEqualTo(DurationUnit.MILLISECOND)
     }
 
     private fun prepareActivitySummary() : FitbitActivitySummary {
@@ -56,6 +137,21 @@ class FitbitActivityMappingTest {
                 description = "Running",
                 hasStartTime = true,
                 duration = 1200000,
+                startDate = LocalDate.of(2020,3,24),
+                startTime = LocalTime.of(8,10,0),
+                steps = 3876
+        )
+    }
+
+    private fun prepareActivityWithDistance() : FitbitActivity {
+        return FitbitActivity(
+                activityId = 1,
+                name = "Outside Running",
+                calories = 345,
+                description = "Running",
+                hasStartTime = true,
+                duration = 1200000,
+                distance = 4.1,
                 startDate = LocalDate.of(2020,3,24),
                 startTime = LocalTime.of(8,10,0),
                 steps = 3876
