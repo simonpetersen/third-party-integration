b807188ce9d667b20bc7c121348d7781fc214d1c
ChristofferSvendsen <s145089@student.dtu.dk>
2020-04-27 13:11:19 +0200

    Test jenkins integration

--- src/main/kotlin/dtu/openhealth/integration/kafka/producer/property/KafkaProducerProperties.kt
+++ src/main/kotlin/dtu/openhealth/integration/kafka/producer/property/KafkaProducerProperties.kt
@@ -1,7 +1,7 @@
 package dtu.openhealth.integration.kafka.producer.property
 
 object KafkaProducerProperties {
-    const val BOOTSTRAP_SERVERS = "localhost:9092"
+    const val BOOTSTRAP_SERVERS = "localhost:9092" // broker:29092
     const val STRING_SERIALIZER = "org.apache.kafka.common.serialization.StringSerializer"
     const val OMH_SERIALIZER = "dtu.openhealth.integration.shared.util.serialization.OmhDTOSerializer"
     const val ACKS = "1"
